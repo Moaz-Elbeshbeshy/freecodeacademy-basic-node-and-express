730051702e90742e36348c67032ace380b4c7bb6
Moaz-Elbeshbeshy <awladnasem800@gmail.com>
2025-04-18 18:38:55 +0200

    Initial commit with Hello World app

--- myApp.js
+++ myApp.js
@@ -4,10 +4,5 @@ let app = express();
 // Log 'Hello World' to the console
 console.log('Hello World');
 
-// Use dynamic port from the environment or default to 5000
-const port = process.env.PORT || 5000;
-
-// Start the server without logging any default messages
-app.listen(port, () => { });  // Remove any callback to suppress default log
 
 module.exports = app;
