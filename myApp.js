132540dea67fc5b9561b9f8734712ef5ab355c1f
Moaz-Elbeshbeshy <awladnasem800@gmail.com>
2025-04-18 18:06:53 +0200

    Updated to use dynamic PORT

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 let express = require('express');
 let app = express();
+
+// Log 'Hello World' to the console
 console.log('Hello World');
 
+// Use dynamic port from the environment or default to 3000
+const port = process.env.PORT || 3000;
 
-
-
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
