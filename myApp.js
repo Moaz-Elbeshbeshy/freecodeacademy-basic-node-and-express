b7a2adf3cbb7e5f58b3316ce1f203401a2a12442
Moaz-Elbeshbeshy <awladnasem800@gmail.com>
2025-04-18 17:43:38 +0200

    removed ! after Hello world

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ let express = require('express');
 let app = express();
 console.log('Hello World!');
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
 
