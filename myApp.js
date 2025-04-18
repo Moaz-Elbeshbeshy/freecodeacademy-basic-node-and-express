ef91980786dda56c8e8183977672e740132a01f7
Moaz-Elbeshbeshy <awladnasem800@gmail.com>
2025-04-18 18:00:26 +0200

    only print Hello World

--- myApp.js
+++ myApp.js
@@ -2,42 +2,6 @@ let express = require('express');
 let app = express();
 console.log('Hello World');
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
